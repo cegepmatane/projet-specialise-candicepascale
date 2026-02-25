2571d9ec0a9195013836aba867484cca2b1e917b
candicepascale <candice2ng@gmail.com>
2026-02-25 09:36:53 -0500

    page boutique qui est encore le catalogue, page categorie, ainsi que la page mission  #6

---
+++ prototype/mission.php
@@ -0,0 +1,47 @@
+<?php
+
+include 'header.php';
+
+?>
+ <h2> Mission </h2>
+    </div>
+    <div class="section s2">
+
+        <table>
+            <caption>
+                Grille de prix
+
+            </caption>
+            <tr>
+                <th colspan="1"> Catégories</th>
+                <th colspan="2"> Noms et Prix Unitaire</th>
+                <th> Prix avec coffret</th>
+            </tr>
+
+            <tr>
+                <td>Hommes</td>
+                <td>Montres</td>
+                <td rowspan="2">100$</td>
+                <td>115$</td>
+            </tr>
+            <tr>
+                <td>Femmes</td>
+                <td>Montres</td>
+                <td>110$</td>
+            </tr>
+            <tr>
+                <td>Enfants</td>
+                <td>Montres</td>
+                <td>50$</td>
+                <td>55$</td>
+            </tr>
+
+        </table>
+
+    </div>
+    <div class="section s3"></div>
+    <div class="section s4"></div>
+    <div class="section s5"></div>
+
+<?php include 'footer.php'; ?>
+
