5f7113e4aeb19fc6c6e53c863b6b41adb70ae326
candicepascale <candice2ng@gmail.com>
2026-02-24 19:35:29 -0500

    index et footer modifié  #7

---
+++ prototype/footer.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<footer>
+        <div class="copyright">
+           Copyright &copy;<span id="annee"></span> Candice Talla.  Tous droits reservés
+        </div>
+
+         <ul class="social-icons">
+             <li>
+               <a href="www.facebook.com"><i class="fa-brands fa-facebook"></i></a>
+             </li>
+             <li>
+               <a href="www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
+             </li>
+             <li>
+               <a href="www.github.com"><i class="fa-brands fa-github"></i></a>
+             </li>
+             <li>
+               <a href="www.whatsapp.com"><i class="fa-brands fa-whatsapp"></i></a>
+             </li>
+             <li><a href="www.twitter.com"><i class="fa-brands fa-twitter"></i></a>
+             </li>
+
+           </ul>
+
+       </footer>
+       <script src="js/general.js" defer></script>
+</body>
+</html>
